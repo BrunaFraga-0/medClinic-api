a992128791aa7fedab6a5fe156b6ca4063dbe134
Bruna Fraga <brunafraga90@gmail.com>
2026-09-12 20:21:07 -0300

    feat: adiciona validação de dados no CreateUserDto e atualiza rota de criação de usuário

--- src/dtos/CreateUserDto.ts
+++ src/dtos/CreateUserDto.ts
@@ -1,23 +1,24 @@
-import { IsString, IsEmail, IsEnum, IsNotEmpty, MaxLength, IsOptional } from 'class-validator';
+import { IsString, IsEmail, IsEnum, IsNotEmpty, MaxLength, IsOptional, IsStrongPassword } from 'class-validator';
 import { UserRole } from '../entities/User';
 
 export class CreateUserDto {
     @IsNotEmpty({ message: 'O campo "name" é obrigatório'})
-    @IsString()
-    @MaxLength(100)
+    @IsString({ message: 'O campo "name" deve ser um texto' })
+    @MaxLength(100, { message: 'O campo "name" deve ter no máximo 100 caracteres' })
     name!: string;
 
     @IsNotEmpty({ message: 'O campo "email" é obrigatório'})
-    @IsEmail()
-    @MaxLength(255)
+    @IsEmail({}, { message: 'Informe um email válido. Exemplo: usuario@email.com' })
+    @MaxLength(255, { message: 'O campo "email" deve ter no máximo 255 caracteres' })
     email!: string;
 
     @IsNotEmpty({ message: 'O campo "password" é obrigatório'})
-    @IsString()
-    @MaxLength(255)
+    @IsString({ message: 'O campo "password" deve ser um texto' })
+    @IsStrongPassword({}, { message: 'A senha deve ter no mínimo 8 caracteres, incluindo letra maiúscula, letra minúscula, número e caractere especial'})
+    @MaxLength(255, { message: 'O campo "password" deve ter no máximo 255 caracteres' })
     password!: string;
 
     @IsOptional()
-    @IsEnum(UserRole)
+    @IsEnum(UserRole, { message: 'O campo "role" deve ser ADMIN ou ATENDENTE' })
     role?: UserRole;
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
 import { UserService } from '../services/UserService';
+import { validateDto } from '../middlewares/validate';
+import { CreateUserDto } from '../dtos/CreateUserDto';
 
 const userRoutes = Router();
 
 const userService = new UserService();
 const userController = new UserController(userService);
 
-userRoutes.post('/users', (req, res) => 
-    userController.createUser(req, res));
+userRoutes.post('/users', 
+    validateDto(CreateUserDto),
+    (req, res) => userController.createUser(req, res));
 
 export default userRoutes;
