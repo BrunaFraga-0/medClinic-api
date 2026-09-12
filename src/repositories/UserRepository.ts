6c188ebf43310eea8ac5361b466bb2cbe5871e74
Bruna Fraga <brunafraga90@gmail.com>
2026-09-12 17:18:39 -0300

    feat: implementa repositório de usuários com método para busca por email

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,8 @@
+import { AppDataSource } from '../database/data-source';
+import { User } from '../entities/User';
+
+export const UserRepository = AppDataSource.getRepository(User).extend({
+    async findByEmail(email: string) {
+        return this.findOneBy({ email });
+    },
+});
