53afb7396b8c4f973cf07444f741ff96deeda63e
Bruna Fraga <brunafraga90@gmail.com>
2026-09-12 17:37:46 -0300

    fix: corrige a porta de conexão do servidor

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 
-const PORT = process.env.DB_PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 AppDataSource.initialize()
 .then(() => {
