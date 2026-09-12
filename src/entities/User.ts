5565f529f6c5f1e7eb104073050c0f6b29f9c762
Bruna Fraga <brunafraga90@gmail.com>
2026-09-11 21:46:04 -0300

    feat: implementa entidade usuários e definição de papeis

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
+
+export enum UserRole{
+    ADMIN = "ADMIN",
+    ATENDENTE = "ATENDENTE"
+};
+
+@Entity("users")
+export class User{
+    @PrimaryGeneratedColumn('uuid')
+    id!: string
+
+    @Column({ type: 'varchar', length: 100 })
+    name!: string
+
+    @Column({ type: 'varchar', unique: true, length: 255 })
+    email!: string
+
+    @Column({ type: 'varchar', length: 255  })
+    password!: string
+
+    @Column({ type: 'enum', enum: UserRole, default: UserRole.ATENDENTE })
+    role!: UserRole
+
+    @CreateDateColumn({name: 'created_At', type: 'timestamp', default: () => 'now()' })
+    createdAt!: Date 
+};
