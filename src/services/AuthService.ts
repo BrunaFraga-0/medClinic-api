971a5c3e43f57fb96c64296c13328fa106129ae0
Bruna Fraga <brunafraga90@gmail.com>
2026-09-13 17:57:44 -0300

    fix: padroniza tratamento de erros nos services (Auth e User)

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -10,29 +10,39 @@ export class AuthService {
     constructor(private userRepository = UserRepository) {};
 
     async login(data: LoginDto): Promise<LoginResponseDto> {
-        const user = await this.userRepository.findByEmail(data.email);
-        if (!user) {
-            throw new AppError('Credenciais inválidas', 401);
+        try{
+
+            const user = await this.userRepository.findByEmail(data.email);
+            if (!user) {
+                throw new AppError('Credenciais inválidas', 401);
+            };
+            
+            const isPasswordValid = await comparePassword(data.password, user.password);
+
+            if (!isPasswordValid) {
+                throw new AppError('Credenciais inválidas', 401);
+            };
+
+            const token = gerarToken({ sub: user.id, role: user.role! });
+
+            const response: LoginResponseDto = {
+                token,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role!
+                }
+            };
+
+            return response;
+
+        } catch (err) {
+            if (err instanceof AppError) {
+                throw err;
+            };
+
+            throw new AppError('Erro interno ao realizar login', 500);
         };
-        
-        const isPasswordValid = await comparePassword(data.password, user.password);
-
-        if (!isPasswordValid) {
-            throw new AppError('Credenciais inválidas', 401);
-        };
-
-        const token = gerarToken({ sub: user.id, role: user.role! });
-
-        const response: LoginResponseDto = {
-            token,
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                role: user.role!
-            }
-        };
-
-        return response;
     };
 };
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -9,47 +9,65 @@ export class UserService {
     constructor(private userRepository = UserRepository) {}
 
     async createUser(data: CreateUserDto): Promise<UserResponseDto> {
-        const existEmail = await this.userRepository.findByEmail(data.email);
-        if (existEmail) {
-            throw new AppError('Já existe um usuário cadastrado com esse email', 409);
+        try{
+            const existEmail = await this.userRepository.findByEmail(data.email);
+            if (existEmail) {
+                throw new AppError('Já existe um usuário cadastrado com esse email', 409);
+            };
+
+            const hashedPassword = await hashPassword(data.password);
+
+            const newUser = await this.userRepository.create({
+                name: data.name,
+                email: data.email,
+                password: hashedPassword,
+                role: data.role
+            });
+
+            const savedUser = await this.userRepository.save(newUser);
+
+            const responseUser: UserResponseDto = {
+                id: savedUser.id,
+                name: savedUser.name,
+                email: savedUser.email,
+                role: savedUser.role!,
+                createdAt: savedUser.createdAt
+            };
+
+            return responseUser;
+        } catch (err) {
+            if (err instanceof AppError) {
+                throw err;
+            };
+
+            throw new AppError('Erro interno ao criar usuário', 500);
         };
-
-        const hashedPassword = await hashPassword(data.password);
-
-        const newUser = await this.userRepository.create({
-            name: data.name,
-            email: data.email,
-            password: hashedPassword,
-            role: data.role
-        });
-
-        const savedUser = await this.userRepository.save(newUser);
-
-        const responseUser: UserResponseDto = {
-            id: savedUser.id,
-            name: savedUser.name,
-            email: savedUser.email,
-            role: savedUser.role!,
-            createdAt: savedUser.createdAt
-        };
-
-        return responseUser;
     };
 
     async getUserById(id: string): Promise<UserResponseDto> {
-        const existId = await this.userRepository.findById(id);
-        if (!existId) {
-            throw new AppError('Usuário não encontrado', 404);
-        };
+        try{
 
-        const responseUserId: UserResponseDto = {
-            id: existId.id,
-            name: existId.name,
-            email: existId.email,
-            role: existId.role!,
-            createdAt: existId.createdAt
-        };
+            const existId = await this.userRepository.findById(id);
+            if (!existId) {
+                throw new AppError('Usuário não encontrado', 404);
+            };
 
-        return responseUserId;
+            const responseUserId: UserResponseDto = {
+                id: existId.id,
+                name: existId.name,
+                email: existId.email,
+                role: existId.role!,
+                createdAt: existId.createdAt
+            };
+
+            return responseUserId;
+            
+        } catch (err) {
+            if (err instanceof AppError) {
+                throw err;
+            };
+
+            throw new AppError('Erro interno ao criar usuário', 500);
+        };
     };
 };
