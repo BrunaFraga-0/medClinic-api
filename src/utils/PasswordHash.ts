8a0399e43a3d17375983528d259c4fa5935d3815
Bruna Fraga <brunafraga90@gmail.com>
2026-09-13 15:17:54 -0300

    feat: implementa utilitarios de autenticação (bcrypt.compare, gerarToken e verificarToken)

--- src/utils/PasswordHash.ts
+++ src/utils/PasswordHash.ts
@@ -3,3 +3,7 @@ import * as bcrypt from 'bcryptjs';
 export async function hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, await bcrypt.genSalt(10));
 };
+
+export function comparePassword(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+};
